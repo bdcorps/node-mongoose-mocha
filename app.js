a6404d340e64730c1ba83ff30e9b92b108961685
Sukhpal Saini <bdcorps@users.noreply.github.com>
2021-12-17 10:21:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ mongoose.connect(uri, {
   useNewUrlParser: true,
 });
 
+// Allow all CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(
   bodyParser.urlencoded({
     extended: false,
